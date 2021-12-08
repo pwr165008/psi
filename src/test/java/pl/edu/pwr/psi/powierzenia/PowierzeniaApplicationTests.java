098a1e18324d9f7e74e44021ff7a71a1d17e1064
Piotr Jewłoszewicz <165008@student.pwr.edu.pl>
2021-12-08 01:49:01 +0100

    Initial

---
+++ src/test/java/pl/edu/pwr/psi/powierzenia/PowierzeniaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.psi.powierzenia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowierzeniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
