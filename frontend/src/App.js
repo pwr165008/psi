900a80982c5e4003c617a5074170efc2ba25407c
Sebastian Siwik <sebastian.siwik11@gmail.com>
2022-02-06 17:52:04 +0100

    Make the login button functional

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './components/home/Home';
-import Login from './components/auth/Login';
-import Logout from './components/auth/Logout';
 
 import './App.css';
 
@@ -12,8 +10,6 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/logout' element={<Logout />} />
         </Routes>
       </BrowserRouter>
     </Layout>
