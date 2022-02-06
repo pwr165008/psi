d32abdb7a839f95210f07de93c574a34ce73ca94
Mateusz Adamczyk <mat.adamczyk@icloud.com>
2022-02-06 17:32:38 +0100

    i18n implementation

--- frontend/src/shared/navbar/Navbar.js
+++ frontend/src/shared/navbar/Navbar.js
@@ -5,8 +5,12 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import LanguageSwitch from "../switch/LanguageSwitch";
+import { useTranslation } from 'react-i18next';
 export const Navbar = () => {
+ 
+  const { t, i18n } = useTranslation();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -20,7 +24,9 @@ export const Navbar = () => {
           >
             <MenuIcon />
           </IconButton>
-          <Button color="inherit">Login</Button>
+         <div sx={{display: 'flex', flexDirection: 'row'}}> 
+          <LanguageSwitch  />
+          <Button color="inherit">{t("login")}</Button></div>
         </Toolbar>
       </AppBar>
     </Box>
