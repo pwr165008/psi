a307c4104462c18e9693182b2a0a489c38da1e08
Mateusz Adamczyk <mat.adamczyk@icloud.com>
2022-02-08 23:28:22 +0100

    Preferences

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './components/home/Home';
+import Preferences from './components/preferences/Preferences';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route path='/preferences' element={<Preferences />} />
         </Routes>
       </BrowserRouter>
     </Layout>
