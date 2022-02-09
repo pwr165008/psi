fd2eabff5eae8921eae08db386b81a52e7685951
Piotr Jewłoszewicz <165008@student.pwr.edu.pl>
2022-02-10 00:31:03 +0100

    Added - JWT validation - endpoint security

---
+++ api/src/main/java/pl/edu/pwr/psi/SecurityConfig.java
@@ -0,0 +1,45 @@
+package pl.edu.pwr.psi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${auth0.audience}")
+    private String audience;
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/templates").permitAll()
+//                .mvcMatchers("/entrustments").authenticated()
+                .antMatchers(HttpMethod.POST, "/entrustments").authenticated()
+//                .mvcMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages")
+               .and().cors()
+                .and().oauth2ResourceServer().jwt().jwkSetUri("https://dev-lz6wlouf.eu.auth0.com/.well-known/jwks.json")
+                .and().and().csrf().disable();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
+    }
+}
