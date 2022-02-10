4d7a32cfa0755c0641f249f4deeae1e72f89a126
Mateusz Adamczyk <mat.adamczyk@icloud.com>
2022-02-10 23:51:06 +0100

    Preferences view

--- frontend/src/components/preferences/Preferences.js
+++ frontend/src/components/preferences/Preferences.js
@@ -6,24 +6,35 @@ import ListItemText from '@mui/material/ListItemText';
 import { FixedSizeList } from 'react-window';
 import { withAuthenticationRequired } from '@auth0/auth0-react';
 
-function renderRow(props) {
+function renderCourseListRow(props) {
   const { index, style } = props;
-
   return (
     <ListItem style={style} key={index} component="div" disablePadding>
       <ListItemButton>
-        <ListItemText primary={`Course ${index + 1}`} />
+        <ListItemText primary={`Course ${index + 1}`}/>
       </ListItemButton>
     </ListItem>
   );
 }
+
+function renderPreferencesListRow(props) {
+  const { index, style } = props;
+  return (
+    <ListItem style={style} key={index} component="div" disablePadding>
+      <ListItemButton>
+        <ListItemText primary={`Course ${index + 1}`}/>
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
 
 export function Preferences() {
   return (
     <><Box
           sx={{ marginTop: '10px', width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
       >
-          <div style={{ backgroundColor: 'lightgrey', fontWeight: 'bold', fontSize: '20px', textAlign: 'center', border: '3px solid #000000' }}>Course List</div>
+          <div style={{ color: 'black' ,backgroundColor: 'lightgrey', fontWeight: 'bold', fontSize: '20px', textAlign: 'center', border: '3px solid #000000' }}>Course List</div>
           <FixedSizeList
               height={400}
               width={360}
@@ -31,20 +42,20 @@ export function Preferences() {
               itemCount={200}
               overscanCount={5}
           >
-              {renderRow}
+              {renderCourseListRow}
           </FixedSizeList>
       </Box><Box
           sx={{ marginTop: '-400px', marginLeft: '650px', width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
       >
-              <div style={{ backgroundColor: 'lightgrey', fontWeight: 'bold', fontSize: '20px', textAlign: 'center', border: '3px solid #000000' }}>Prefered Courses</div>
-              <FixedSizeList
+              <div style={{ color: 'black' ,backgroundColor: 'lightgrey', fontWeight: 'bold', fontSize: '20px', textAlign: 'center', border: '3px solid #000000' }}>Prefered Courses</div>
+              <FixedSizeList 
                   height={400}
                   width={360}
                   itemSize={46}
-                  itemCount={200}
+                  itemCount={200} 
                   overscanCount={5}
               >
-                  {renderRow}
+                  {renderPreferencesListRow}
               </FixedSizeList>
           </Box></>
   );
