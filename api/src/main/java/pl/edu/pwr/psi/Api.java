3555c8a34bd0f4176cd26de5cc027b3440ea30fd
Piotr Jewłoszewicz <165008@student.pwr.edu.pl>
2022-02-11 00:48:57 +0100

    sonar

--- api/src/main/java/pl/edu/pwr/psi/Api.java
+++ api/src/main/java/pl/edu/pwr/psi/Api.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {
         "pl.edu.pwr.psi.entrustments",
         "pl.edu.pwr.psi.preferences",
-        "pl.edu.pwr.psi.templates",
-//        "pl.edu.pwr.psi",
 })
 public class Api {
     public static void main(String[] args) {
