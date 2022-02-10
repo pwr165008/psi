aaad15bcba1076cb888c79458e1e0833eae71e1e
Sebastian Siwik <sebastian.siwik11@gmail.com>
2022-02-10 20:10:26 +0100

    Simplify auth context

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,15 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import MenuBox from './MenuBox';
-import authContext from '../../context/authContext';
 import MenuSkeleton from './MenuSkeleton';
-import { withAuthenticationRequired } from '@auth0/auth0-react';
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
 import roleMenus from "./role-menus.json";
 
 export const Home = () => {
-    const userData = useContext(authContext);
-    const roles = userData && userData['https://psi.empinet.pl/roles'];
+    const { user } = useAuth0();
+    const roles = user && user['https://psi.empinet.pl/roles'];
 
     return (
         <div>
