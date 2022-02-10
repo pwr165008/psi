abb005421354fbc195f9f49fe27038bc16be86eb
Sebastian Siwik <sebastian.siwik11@gmail.com>
2022-02-10 22:30:20 +0100

    Add basic operator entrustments

---
+++ frontend/src/components/entrustments/OperatorEntrustments.js
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+import authContext from '../../context/authContext';
+import axios from '../../axios';
+
+export const OperatorEntrustments = () => {
+
+    const context = useContext(authContext);
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        if (context) {
+            axios.post('entrustments', null, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${context?.token}`,
+                    'Mode': 'no-cors'
+                }
+            }).then(res => setData(res.data));
+        }
+    }, [data, setData, context]);
+
+    return (
+        <div>
+            {data?.entrustments.map((x, i) => (<p key={i}>{x.lecturerId}</p>))}
+        </div>
+    );
+}
+
+export default withAuthenticationRequired(OperatorEntrustments);
